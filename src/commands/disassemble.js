5cf4128c559df83e551663386865787b3715cfa7
Nicky Nickell <pnqcad@gmail.com>
2020-04-19 20:03:32 -0400

    Implement disassemble command

--- src/commands/disassemble.js
+++ src/commands/disassemble.js
@@ -1,10 +1,36 @@
+const fs = require('fs');
+const PapyrusScript = require('./../script/PapyrusScript');
+
 exports.command = 'disassemble <file>';
 exports.desc = 'disassemble a pex file';
 exports.builder = (yargs) => {
   yargs.positional('file', {
     type: 'string',
     describe: 'the file to disassemble'
+  })
+  .option('output', {
+    alias: 'o',
+    type: 'string',
+    describe: 'file to write output to'
   });
 };
 exports.handler = (argv) => {
+  let {file, output} = argv;
+
+  if (!file.endsWith('.pex'))
+    file += '.pex';
+
+  if (!output) {
+    output = file.substring(0, file.length - 4) + '.pas';
+  } else if (!output.endsWith('.pas')) {
+    output += '.pas';
+  }
+
+  if (!fs.existsSync(file)) {
+    console.log('input file does not exist');
+    return;
+  }
+
+  let script = PapyrusScript.load(file);
+  fs.writeFileSync(output, script.asPas());
 }
