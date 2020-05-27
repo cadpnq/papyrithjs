f1be9c0211d417303d2289fd8f7a873023bab778
Nicky Nickell <pnqcad@gmail.com>
2020-05-27 17:56:51 -0400

    Fix incorrectness in binding analyzer

--- src/pex/Binding.js
+++ src/pex/Binding.js
@@ -12,39 +12,55 @@ module.exports = class Binding {
   constructor(code, index, bindings) {
     this.instruction = code[index];
     this.to = this.instruction.dest.nvalue;
-    this.uses = this._getUses(code, index + 1);
+    this.ends = [];
+    this.uses = [];
+    this._getUses(code, index + 1);
+    this._getEnds(code);
     this.bindings = bindings;
     this.index = index;
+  }
+
+  _getEnds(code) {
+    for (let use of this.uses) {
+      let nextInstruction = code[code.indexOf(use) + 1];
+      if (use.op != 'jump' &&
+          (!this.uses.includes(nextInstruction) ||
+           use == this.instruction)) {
+        this.ends.unshift(use);
+      }
+    }
+    if (!this.ends.length) this.ends.push(this.instruction);
   }
 
   _getUses(code, index, visited = new Set()) {
     let instruction = code[index];
     if (!instruction || visited.has(instruction)) {
-      return [];
+      return;
     }
     visited.add(instruction);
 
-    let uses = [];
-    let next = [];
     if (instruction.target) {
-      uses = this._getUses(code, target(instruction.target, code), visited);
+      this._getUses(code, target(instruction.target, code), visited);
     }
 
-    if (code[index + 1]) {
+    let nextInstruction = code[index + 1];
+
+    if (nextInstruction &&
+        !['jump', 'return'].includes(instruction.op)) {
       if (instruction.dest) {
         if (instruction.dest.nvalue != this.to) {
-          next = this._getUses(code, index + 1, visited);
+          this._getUses(code, index + 1, visited);
         }
       } else {
-        next = this._getUses(code, index + 1, visited);
+        this._getUses(code, index + 1, visited);
       }
     }
 
-    if (next.length || usesId(this.to, instruction)) {
-      uses.push(instruction);
+    if (usesId(this.to, instruction) ||
+        this.uses.includes(code[target(instruction.target, code)]) ||
+        this.uses.includes(nextInstruction)) {
+      this.uses.unshift(instruction);
     }
-
-    return uses.concat(next);
   }
 
   static allBindings(code) {
@@ -58,7 +74,21 @@ module.exports = class Binding {
   }
 
   intersects(binding) {
-    return new Set([...this.uses, ...binding.uses]).size != this.uses.length + binding.uses.length;
+    for (let instruction of [binding.instruction, ...binding.uses]) {
+      if (this.uses.includes(instruction) &&
+          !this.ends.includes(instruction)) {
+        return true;
+      }
+    }
+
+    for (let instruction of [this.instruction, ...this.uses]) {
+      if (binding.uses.includes(instruction) &&
+          !binding.ends.includes(instruction)) {
+        return true;
+      }
+    }
+
+    return false;
   }
 
   siblings() {
@@ -81,5 +111,9 @@ module.exports = class Binding {
       });
     }
     this.to = id.nvalue;
+  }
+
+  usesId(id) {
+    return this.uses.some((i) => usesId(id, i));
   }
 }
